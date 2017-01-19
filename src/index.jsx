812a4e29ea4e3c477be79c6fb1af1e913f9f68ca
Nicolas Agustin Torres <nicolastrres@gmail.com>
2017-01-18 21:04:28 -0300

    Add react and create basic component to test configurations

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,10 @@
-console.log('Hello world');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render () {
+    return <p> Hello React!</p>;
+  }
+}
+
+render(<App/>, document.getElementById('main'));
